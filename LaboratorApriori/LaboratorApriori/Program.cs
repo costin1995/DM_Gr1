1f61e8872f34138a5fdb25cf9889591c62c61002
DESKTOP-3FN9FV0\badiu <badiu.bogdan.vasile@gmail.com>
2020-10-20 17:36:03 +0300

    Am creat proiectul.

---
+++ LaboratorApriori/LaboratorApriori/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratorApriori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("!!!!Hello World si spor la scris, dragi mei coechipieri!!!!!");
+            Console.ReadLine();
+        }
+    }
+}
