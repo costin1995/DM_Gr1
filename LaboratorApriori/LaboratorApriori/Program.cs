75fa55377553edba1130868e023ac8f9baac33c1
cojocaru161 <cojocaru161@gmail.com>
2020-10-20 18:15:17 +0300

    Am adaugat metoda ReadCSVFile(string cale)

--- LaboratorApriori/LaboratorApriori/Program.cs
+++ LaboratorApriori/LaboratorApriori/Program.cs
@@ -8,9 +8,34 @@ namespace LaboratorApriori
 {
     class Program
     {
+        static string[,] ReadCSVFile(string cale)
+        {
+
+            string[] dateFisier = System.IO.File.ReadAllLines(cale);
+
+            int randuri = dateFisier.Length;
+            int coloane = dateFisier[0].Split(',').Length;
+
+            string[,] continutCelule = new string[randuri, coloane];
+
+            for(int i = 0; i < randuri; i++)
+            {
+                string[] linie = dateFisier[i].Split(',');
+                for(int j = 0; j < coloane; j++)
+                {
+                    continutCelule[i, j] = linie[j];
+                   
+                }
+
+            }
+
+            return continutCelule;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("!!!!Hello World si spor la scris, dragi mei coechipieri!!!!!");
+            ReadCSVFile(@"test_59_2.csv");
             Console.ReadLine();
         }
     }
