4ad58cde26218c8b2a456ad639c43ebe16db6a28
cojocaru161 <cojocaru161@gmail.com>
2020-11-09 21:03:26 +0200

    Metode XML

--- Extragerea Trasaturilor/Extragerea Trasaturilor/Article.cs
+++ Extragerea Trasaturilor/Extragerea Trasaturilor/Article.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace Extragerea_Trasaturilor
         private string Tile;
         private string Text;
         private List<string> ClassCodes = new List<string>();
+
 
         public Article(string tile1, string text1, List<string> classCodes1)
         {
@@ -52,8 +54,11 @@ namespace Extragerea_Trasaturilor
         public string GetXmlNodeContentByName(XmlDocument Obj, string NumeNod)
         {
             string Info = "";
-            
-            
+
+            foreach(XmlNode xmlNode in Obj)
+            {
+                Info += xmlNode[NumeNod].InnerText;
+            }
 
 
             return Info;
@@ -62,6 +67,16 @@ namespace Extragerea_Trasaturilor
         public List<string> GetClassCodesFromXml(XmlDocument Obj)
         {
             List<string> NoduriCodesCareContinClasaBipTopics = new List<string>();
+            
+
+            foreach (XmlNode xmlNode in Obj.SelectNodes("codes"))
+            {
+                if (xmlNode.Attributes.Equals("bip:topics:1.0"))
+                {
+                    string aux = xmlNode.ChildNodes.ToString();
+                    NoduriCodesCareContinClasaBipTopics.Add(aux);
+                }
+            }
 
 
             return NoduriCodesCareContinClasaBipTopics;
@@ -70,6 +85,25 @@ namespace Extragerea_Trasaturilor
         public List<Article> VerificareSiInstantiereFisiereXml(string CaleRelativaCatreFolderCuFisiere)
         {
             List<Article> ListaFisiereXml = new List<Article>();
+            string cale = "./../../InputData/" + CaleRelativaCatreFolderCuFisiere;
+            XmlDocument xmlDocument = new XmlDocument();
+
+
+            string[] AllDirectories = Directory.GetFiles(cale, "*", SearchOption.AllDirectories);
+            
+            for(var i = 0; i < AllDirectories.Length; i++)
+            {
+                if(AllDirectories[i].EndsWith("xml"))
+                {
+                    xmlDocument.LoadXml(AllDirectories[i]);
+                    Article obj = new Article(GetXmlNodeContentByName(xmlDocument,"title"),
+                                              GetXmlNodeContentByName(xmlDocument,"text"),
+                                              GetClassCodesFromXml(xmlDocument));
+
+                    ListaFisiereXml.Add(obj);
+                }
+            }
+
 
             return ListaFisiereXml;
         }
